1592666f6ef2609830b8a784e4f560ed76ac0b88
AlexanderRodin <pass.9157@gmail.com>
2023-08-21 16:19:07 +0300

    snackbarHostState

--- app/src/main/java/com/example/firstcomposeprodject/ui/theme/VkNewsMainScreen.kt
+++ app/src/main/java/com/example/firstcomposeprodject/ui/theme/VkNewsMainScreen.kt
@@ -1,24 +1,63 @@
 package com.example.firstcomposeprodject.ui.theme
 
 import android.annotation.SuppressLint
+import android.util.Log
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.NavigationBarItemDefaults
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarDuration
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.SnackbarResult
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.res.stringResource
+import kotlinx.coroutines.launch
 
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MainScreen() {
+    val snackbarHostState = remember {
+        SnackbarHostState()
+    }
+    Log.d("MainScreen", snackbarHostState.currentSnackbarData.toString())
+    val scope = rememberCoroutineScope()
+    val fabIsVisible = remember {
+        mutableStateOf(true)
+    }
     Scaffold(
+        snackbarHost = {
+            SnackbarHost(hostState = snackbarHostState)
+        },
+        floatingActionButton = {
+            if (fabIsVisible.value) {
+                FloatingActionButton(onClick = {
+                    scope.launch {
+                        val action = snackbarHostState.showSnackbar(
+                            message = "This is snackbar",
+                            actionLabel = "Hide FAB",
+                            duration = SnackbarDuration.Long
+                        )
+                        if (action == SnackbarResult.ActionPerformed) {
+                            fabIsVisible.value = false
+                        }
+                    }
+                }) {
+                    Icon(Icons.Filled.Favorite, contentDescription = null)
+                }
+            }
+        },
         bottomBar = {
             NavigationBar {
                 val selectedItemPosition = remember {
@@ -33,7 +72,7 @@ fun MainScreen() {
                     NavigationBarItem(
                         selected = selectedItemPosition.value == index,
                         onClick = {
-                                  selectedItemPosition.value = index
+                            selectedItemPosition.value = index
                         },
                         icon = { Icon(item.icon, null) },
                         label = { Text(text = stringResource(id = item.titleResId)) },
