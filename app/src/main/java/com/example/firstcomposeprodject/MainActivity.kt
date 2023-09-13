5454c8f4f95bad9317fc1dacdb6e002373a8a3e5
AlexanderRodin <pass.9157@gmail.com>
2023-09-13 18:29:38 +0300

    add VK API in project

--- app/src/main/java/com/example/firstcomposeprodject/MainActivity.kt
+++ app/src/main/java/com/example/firstcomposeprodject/MainActivity.kt
@@ -1,88 +1,37 @@
 package com.example.firstcomposeprodject
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
-import androidx.activity.viewModels
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
-import androidx.compose.material3.DismissDirection
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.SwipeToDismiss
-import androidx.compose.material3.Text
-import androidx.compose.material3.rememberDismissState
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.livedata.observeAsState
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.example.firstcomposeprodject.ui.theme.FirstComposeProjectTheme
-import com.example.firstcomposeprodject.ui.theme.InstagramProfileCard
 import com.example.firstcomposeprodject.ui.theme.MainScreen
+import com.vk.api.sdk.VK
+import com.vk.api.sdk.auth.VKAuthenticationResult
+import com.vk.api.sdk.auth.VKScope
 
 
 class MainActivity : ComponentActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             FirstComposeProjectTheme {
-                MainScreen()
-            }
-        }
-    }
-}
-
-@OptIn(ExperimentalMaterial3Api::class)
-@Composable
-private fun Test(viewModel: NewsFeedViewModel) {
-    FirstComposeProjectTheme {
-        Box(
-            modifier = Modifier.fillMaxSize(),
-        ) {
-            val models = viewModel.models.observeAsState(listOf())
-            LazyColumn()
-            {
-                items(models.value, key = { it.id }) {
-                    val dismissState = rememberDismissState()
-                    if (dismissState.isDismissed(DismissDirection.EndToStart)) {
-                        viewModel.delete(it)
-                    }
-                    SwipeToDismiss(
-                        state = dismissState,
-                        directions = setOf(DismissDirection.EndToStart),
-                        background = {
-                            Box(
-                                modifier = Modifier
-                                    .padding(16.dp)
-                                    .fillMaxSize()
-                                    .background(Color.Red.copy(alpha = 0.5f)),
-                                contentAlignment = Alignment.CenterEnd
-                            ) {
-                                Text(
-                                    modifier = Modifier.padding(16.dp),
-                                    text = "Delete item",
-                                    color = Color.White,
-                                    fontSize = 24.sp
-                                )
-                            }
-                        },
-                        dismissContent = {
-                            InstagramProfileCard(
-                                model = it,
-                                onFollowButtonClickListener = {
-                                    viewModel.changeFollowingStatus(it)
-                                }
-                            )
+                val launcher = rememberLauncherForActivityResult(
+                    contract = VK.getVKAuthActivityResultContract()
+                ) {
+                    when (it) {
+                        is VKAuthenticationResult.Success -> {
+                            Log.d("MainActivity", "Success auth")
                         }
-                    )
+
+                        is VKAuthenticationResult.Failed -> {
+                            Log.d("MainActivity", "Failed auth")
+                        }
+                    }
                 }
+                launcher.launch(listOf(VKScope.WALL))
+                MainScreen()
             }
         }
     }
